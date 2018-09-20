d39ea0c677df73624e8aa4b3adb6158710834b00
Meagan Crowley <meagan.crowley001@gmail.com>
2018-09-19 19:53:46 -0600

    Replace filter function with lambda

--- marvin_the_depressed_bot.py
+++ marvin_the_depressed_bot.py
@@ -11,12 +11,6 @@ subreddit = reddit.subreddit("pythonforengineers")
 marvin_quotes = []
 
 
-def things_to_filter(x):
-    if len(x) == 0 or x[0] == "#":
-        return False
-    return True
-
-
 """ Searches through recent comments for 'Marvin Help' and replies.
 Stops after 5 requests that result in no new items """
 for comment in subreddit.stream.comments(pause_after=5):
@@ -25,7 +19,8 @@ for comment in subreddit.stream.comments(pause_after=5):
     with open("marvin_quotes.txt", "r") as f:
         marvin_quotes = f.read()
         marvin_quotes = marvin_quotes.split("\n")
-        marvin_quotes = list(filter(things_to_filter, marvin_quotes))
+        marvin_quotes = list(filter(lambda x: False if len(x) == 0 or
+                             x[0] == "#" else True, marvin_quotes))
     if re.search("Marvin Help", comment.body, re.IGNORECASE):
         marvin_reply = "Marvin the Depressed Robot says: " \
                      + random.choice(marvin_quotes)
